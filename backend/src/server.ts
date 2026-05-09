045c9b56ef4f0906bb69992b39e2b73aa605975b
Erandhi Madhushika <madhushikabae.20@uom.lk>
2026-05-09 10:51:15 +0530

    Update backend server with socket.io

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,19 @@
 import { createServer } from "http";
-import app from "./app";
-import { env } from "./config/env";
+import { Server } from "socket.io";
+import app from "../src/app";
+import { env } from "../src/config/env";
+import { initSockets } from "../src/sockets";
 
 const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+initSockets(io);
 
 httpServer.listen(env.PORT, () => {
   console.log(`Server running on http://localhost:${env.PORT}`);
